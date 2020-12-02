a0d6184cdd1d81afacd16e9ea9d7a02256bbb0e9
jkordonski <jkordonski97@gmail.com>
2020-12-02 21:12:41 +0000

    zad 1-3

--- lab5.cpp
+++ lab5.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int main()
 {
-    puts("Hello, World!");
+
+    // zad 1 i 2
+
+    /*
+      vector< int > vec = {};
+      cout << "my vector" << endl;
+      cout << "size: " << vec.size() << endl;
+      cout << "capacity: " << vec.capacity() << endl;
+      for (unsigned int i = 0; i < 100; i++) {
+          vec.push_back(i);
+          cout << i << ". "
+               << "size: " << vec.size() << " capacity: " << vec.capacity()
+               << " adres pierwszego elementu:" << &vec[0] << endl;
+      }
+    */
+
+    // zad 3
+    /*
+    vector< int > vec1{100, 42};
+    cout << "size: " << vec1.size() << endl;
+    cout << "capacity: " << vec1.capacity() << endl;
+    */
+
+    
 }
